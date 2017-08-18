e85777bb4185641a46457a51c5c2e7f1328a8a69
Sandesh Kalantre <kalantresandesh@gmail.com>
2017-08-18 23:18:17 +0530

    working on the Hamiltonian construction

--- nanowire_model/physics.py
+++ nanowire_model/physics.py
@@ -11,19 +11,48 @@ class Physics:
     phase : phase-difference between the superconducting contacts
     (mu1,mu2) : chemical potential of the two contacts
     N_D : number of points in the device
+    N_int : number of internal degrees of freedom
     '''
+
     def __init__(self,param):
         self.param = param
 
-    def create_Hamiltonian(self):
+    def create_alpha(self):
+        '''
+        Input:
+        None
+
+        Output:
+        Return the alpha matrix, the site element in the Hamiltonian
+        '''
+
         t0 = self.param['t0']
-        N_D = self.param['N_D']
-
-
         alpha = np.array([[2*t0,0],[0,2*t0]])
+
+        return alpha
+
+    def create_beta(self): 
+        '''
+        Input:
+        None
+
+        Output:
+        Return the beta matrix, the nearest neighbour coupling element in the Hamiltonian
+        '''
+        t0 = self.param['t0']
         beta = np.array([[-t0,0],[0,-t0]])
         
-        H = np.zeros((N_D,N_D,2,2))
+        return beta
+
+    def create_Hamiltonian(self):
+        N_D = self.param['N_D']
+        N_int = self.param['N_int'] 
+        
+
+        H = np.zeros((N_D,N_D,N_int,N_int))
+        alpha = self.create_alpha()
+        beta = self.create_beta()
+
         # desperation, I can't find a better way than to loop over
         for i in range(N_D):
             H[i,i] = alpha
@@ -33,8 +62,9 @@ class Physics:
 
             if i < (N_D-1):
                 H[i+1,i] = beta 
-
+        
         self.H = H
+
     
 
         
